390ffe384d80d3b94aa31956354155f5c261f2fb
Mauro Castro <maurocastruli@gmail.com>
2025-10-09 15:57:23 -0300

    empujar cajas

--- mistborn/Personaje/char_1.gd
+++ mistborn/Personaje/char_1.gd
@@ -25,7 +25,12 @@ var dash_elapsed: float = 0.0
 var post_dash_timer: float = 0.0
 
 var metal_body: Node2D = null
+var metal_box: Node2D = null
 var can_dash: bool = false
+var can_push_box:bool = false
+var es_liviano:bool = false
+var es_pesado:bool = false
+var push_box_force:float = 500
 
 @onready var sensor_metales: Area2D = $sensor_metales
 @onready var sensor_metales_shape: CollisionShape2D = $sensor_metales/collisionShape
@@ -123,42 +128,75 @@ func _start_pull() -> void:
 func _start_push() -> void:
 	if not (can_dash and is_instance_valid(metal_body)):
 		return
-	var dir := (global_position - metal_body.global_position).normalized()
-	_begin_dash(dir, push_speed)
+	if can_dash and es_pesado:
+		var dir := (global_position - metal_body.global_position).normalized()
+		_begin_dash(dir, push_speed)
+	elif can_push_box and es_liviano and is_instance_valid(metal_box):
+		var dir := (metal_box.global_position - global_position).normalized() # del jugador a la caja
+		_push_boxes(dir, metal_box)
+
+func _push_boxes(dir, body:RigidBody2D):
+	var impulse = dir * push_box_force
+	body.apply_impulse(impulse)
+	body.apply_torque_impulse(impulse.length() * 0.1)
+
+	print("Empujando caja:", body.name, " con impulso ", impulse)
 
 func _begin_dash(dir: Vector2, speed: float) -> void:
 	is_dashing = true
 	dash_dir = dir
 	dash_speed = speed
 	dash_elapsed = 0.0
-	# Opcional: cancelá componente vertical previa para que el dash sea más “limpio”
 	velocity.y = 0.0
 
 func _stop_dash() -> void:
 	is_dashing = false
-	# IMPORTANTE: NO reseteamos velocity ni dirección.
-	# Conservamos exactamente la velocidad final del dash.
 	post_dash_timer = post_dash_cooldown
 
 # Señales del sensor de metales (Area2D)
 func _on_sensor_metales_body_entered(body: Node2D) -> void:
 	if body.is_in_group("metal"):
-		can_dash = true
-		metal_body = body
-		if texto_metal:
-			texto_metal.text = "hay metales"
-			texto_metal.visible = true
-		line.visible = true
-		print("hay colision con metal en:", body.global_position)
+		_entro_metal(body)
+		if body.is_in_group("pesado"):
+			es_pesado = true
+			print("es pesado" , body)
+	if body.is_in_group("cajas"):
+		_entro_caja(body)
+		if body.is_in_group("liviano"):
+			es_liviano = true
+			print("es liviano" , body)
 
 func _on_sensor_metales_body_exited(body: Node2D) -> void:
 	if body == metal_body:
-		metal_body = null
-		can_dash = false
-		if texto_metal:
-			texto_metal.visible = false
-		line.visible = false
-		line.clear_points()
+		_salio_metal(body)
+		if body.is_in_group("pesado"):
+			es_pesado = false
+	if body == metal_box:
+		metal_box = null
+		if body.is_in_group("liviano"):
+			es_liviano = false
+		
+func _entro_metal(body):
+	can_dash = true
+	metal_body = body
+	if texto_metal:
+		texto_metal.text = "hay metales"
+		texto_metal.visible = true
+	line.visible = true
+
+func _salio_metal(_body):
+	metal_body = null
+	can_dash = false
+	if texto_metal:
+		texto_metal.visible = false
+	line.visible = false
+	line.clear_points()
+
+func _entro_caja(body:RigidBody2D):
+	metal_box = body
+	can_push_box = true
+	print("entro caja - se puede empujar")
+
 
 func _draw() -> void:
 	if not mostrar_sensor_debug:
